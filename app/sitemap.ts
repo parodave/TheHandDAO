15329e44082099020a5a38935c784dc02251b623
Dave <123788157+parodave@users.noreply.github.com>
2025-08-28 12:39:59 +0700

    chore: simplify app and remove i18n

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,28 +1,12 @@
-import { MetadataRoute } from 'next';
-import { locales } from './i18n/config';
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://kr-thehand.com';
-  
-  const routes = [
-    '',
-    '/dao',
-    '/contact',
-    '/legal',
+  return [
+    {
+      url: "https://kr-thehand.com/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
   ];
-
-  const sitemap: MetadataRoute.Sitemap = [];
-
-  for (const locale of locales) {
-    for (const route of routes) {
-      sitemap.push({
-        url: `${baseUrl}/${locale}${route}/`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: route === '' ? 1 : 0.8,
-      });
-    }
-  }
-
-  return sitemap;
 }
